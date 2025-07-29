a5fbbfd845cbd82a97658ff3dfd6ac95b837910f
shAn-kor <kjn0406@naver.com>
2025-07-30 00:01:20 +0900

    git init

---
+++ src/test/java/com/example/opensourceproxy/OpensourceProxyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.opensourceproxy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpensourceProxyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
